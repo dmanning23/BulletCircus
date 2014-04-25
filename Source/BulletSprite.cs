6ba280bb62e56e7930b30ee6c07f144e9bdf6c25
unknown <dmanning23@hotmail.com>
2014-04-24 22:06:14 -0500

    added bulletsprite

---
+++ Source/BulletSprite.cs
@@ -0,0 +1,54 @@
+using BulletMLLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace BulletCircus
+{
+	/// <summary>
+	/// This thing takes a bullet and a texture2d and draws the tex over the bullet.
+	/// </summary>
+	public class BulletSprite
+	{
+		#region Properties
+
+		/// <summary>
+		/// The texture to draw over the bullets
+		/// </summary>
+		Texture2D Tex { get; set; }
+
+		#endregion //Properties
+
+		#region Methods
+
+		public BulletSprite(Texture2D tex)
+		{
+			Tex = tex;
+		}
+
+		/// <summary>
+		/// draw a bunch of bullets
+		/// </summary>
+		/// <param name="spriteBatch"></param>
+		/// <param name="bullets"></param>
+		public void Render(SpriteBatch spriteBatch, List<Bullet> bullets)
+		{
+			for (int i = 0; i < bullets.Count; i++)
+			{
+				Render(spriteBatch, bullets[i]);
+			}
+		}
+
+		/// <summary>
+		/// draw a single bullet
+		/// </summary>
+		/// <param name="spriteBatch"></param>
+		/// <param name="bullet">the bullet to draw</param>
+		public void Render(SpriteBatch spriteBatch, Bullet bullet)
+		{
+			//TODO: draw the bullet
+		}
+
+		#endregion //Methods
+	}
+}
