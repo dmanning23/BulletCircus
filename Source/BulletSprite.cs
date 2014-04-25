38b23ec32b91305feaf45560756876924677468f
Daniel Manning <daniel.manning@demandmedia.com>
2014-04-25 17:55:46 -0500

    master: a-0 render a texture over the bullets

--- Source/BulletSprite.cs
+++ Source/BulletSprite.cs
@@ -1,4 +1,5 @@
 using BulletMLLib;
+using MatrixExtensions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
@@ -17,6 +18,8 @@ namespace BulletCircus
 		/// </summary>
 		Texture2D Tex { get; set; }
 
+		Vector2 Offset { get; set; }
+
 		#endregion //Properties
 
 		#region Methods
@@ -24,6 +27,8 @@ namespace BulletCircus
 		public BulletSprite(Texture2D tex)
 		{
 			Tex = tex;
+
+			Offset = new Vector2(Tex.Bounds.Width * -0.5f, Tex.Bounds.Height * -0.5f);
 		}
 
 		/// <summary>
@@ -46,7 +51,30 @@ namespace BulletCircus
 		/// <param name="bullet">the bullet to draw</param>
 		public void Render(SpriteBatch spriteBatch, Bullet bullet, Color color)
 		{
-			//TODO: draw the bullet
+			//get the location to draw at:
+			
+			//get the offset vector
+			Vector2 pos = new Vector2((Offset.X * bullet.Scale), (Offset.Y * bullet.Scale));
+
+			//rotate the offset
+			var matrix = MatrixExt.Orientation(bullet.Direction);
+			pos = matrix.Multiply(pos);
+
+			//move to the correct location
+			pos += new Vector2(bullet.X, bullet.Y);
+
+			//draw the bullet
+			spriteBatch.Draw(Tex,
+				pos,
+				null,
+				color,
+				bullet.Direction,
+				Vector2.Zero,
+				bullet.Scale,
+				SpriteEffects.None,
+				0.0f);
+
+
 		}
 
 		#endregion //Methods
