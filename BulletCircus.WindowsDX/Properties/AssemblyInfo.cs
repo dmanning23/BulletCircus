fc57e2d6db6a89421704a60ccb7bc51c85144e3e
Dan Manning <dmanning23@hotmail.com>
2018-02-20 23:09:57 -0600

    updated nugets

--- BulletCircus.WindowsDX/Properties/AssemblyInfo.cs
+++ BulletCircus.WindowsDX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BulletCircus.DesktopGL")]
+[assembly: AssemblyTitle("BulletCircus.WindowsDX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BulletCircus.DesktopGL")]
+[assembly: AssemblyProduct("BulletCircus.WindowsDX")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
